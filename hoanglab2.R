8d71083292d6d1e4606fba83415d1dcbdddd2665
Christine Hoang <christinehoangt@gmail.com>
2024-01-20 15:04:51 -0600

    initsave

---
+++ hoanglab2.R
@@ -0,0 +1,43 @@
+# Populate the variables below and then run the script to make sure you didn't make any mistakes. 
+# Answer the four questions below using comments.
+# When you are finished save this script off as an R script with a naming convention of lastnamelab1.R
+# Submit this file for grading.
+# store your first name in the first variable
+first <- "Christine"
+# store your last name in the last variable
+last <- "Hoang"
+lackeyAge <- 62
+# store your current age in the yourAge variable
+yourAge <- 34
+# store your favorite movis in the movie variable
+movie <- "The Graduate"
+period <- "."
+# store your favorite hobby in the hobby variable
+hobby <- "make music"
+# store your favorite color in the color variable
+color <- "blue"
+text1 <- "Hello my name is"
+text2 <- ", it's nice to meet you"
+text3 <- "I am "
+text4 <- "years younger than Mr. Lackey"
+text5 <- "The last good movie I saw was"
+text6 <- "My favorite color is"
+text7 <- "In my spare time I like to"
+text8 <- "Have a great day!"
+paste(text1, first, last, text2, period, text3, lackeyAge - yourAge, text4, period, text5, movie, period, text6, color, period, text7, hobby, period, text8)
+
+# Question 1. How many labs are contained in this course? 
+question1<-"10"
+# Question 2. Will a case study will be done towards the end of the semester?
+question2<-"Yes"
+# Question 3. How many points will you need to earn in order to get an A in this course? 
+question3<-"900-1000"
+# Question 4. How many graded discussion posts are there in this course? 
+question4<-"Students are expected to ask questions, participate in set, online discussion sessions as needed"
+
+test9<-"There are"
+test10<-"labs in this course"
+test11<-",there will be a case study"
+test12<-"You need"
+test13<- "points to get an A"
+paste(test9, question1,test10,period, question2, test11, period, test12, question3, test13, period, question4, period)
